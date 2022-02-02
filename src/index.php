b57f610d97929ab901c477dc9c926db5695981bf
Damir S <damir23.02@mail.ru>
2022-02-02 23:46:00 +0300

    minor bug fixed

---
+++ src/index.php
@@ -0,0 +1,239 @@
+<?require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');?>
+
+<!-- Main::Start-->
+<style>
+    #page-content {
+    position: relative;
+    padding: 100px 0 100px;
+  }
+
+  @media (min-width: 1024px) {
+    #page-content {
+      padding: 0 0 100px;
+    }
+  }
+
+  #page-content h1:not(.logo-h1),
+  #page-content h2 {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    color: #606060;
+    margin: 0;
+    text-align: center;
+  }
+
+  #page-content h2 {
+    margin: 27px 0;
+    font-size: 27px;
+    line-height: 1.25;
+  }
+
+  @media (min-width: 768px) {
+    #page-content h2 {
+      font-size: 27px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    #page-content h2 {
+      font-size: 27px;
+      line-height: 1.4;
+    }
+  }
+
+  #page-content h1 {
+    font-size: 27px;
+    line-height: 1.25;
+  }
+
+  @media (min-width: 768px) {
+    #page-content h1 {
+      font-size: 36px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    #page-content h1 {
+      font-size: 42px;
+      line-height: 1.4;
+    }
+  }
+
+  #page-content img {
+    width: unset;
+    max-width: 100%;
+    height: auto;
+    vertical-align: top;
+  }
+
+  @media (max-width: 767px) {
+    #page-content h1 {
+      margin-bottom: 0.3em;
+    }
+  }
+
+  @media (max-width: 767px) {
+    #page-content h2 {
+      margin-bottom: 0.2em;
+    }
+  }
+
+  #page-content .content {
+    background: #ffffff;
+    padding: 50px;
+    zoom: 1;
+    -webkit-border-radius: 2px;
+    -ms-border-radius: 2px;
+    -moz-border-radius: 2px;
+    -o-border-radius: 2px;
+    border-radius: 2px;
+  }
+
+  #page-content .content:before {
+    content: "";
+    display: table;
+  }
+
+  #page-content .content:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  @media (max-width: 767px) {
+    #page-content .content {
+      -webkit-border-radius: 0;
+      -ms-border-radius: 0;
+      -moz-border-radius: 0;
+      -o-border-radius: 0;
+      border-radius: 0;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    #page-content .content {
+      padding: 30px 20px;
+    }
+  }
+
+  .feature-header {
+    line-height: 1.25em;
+  }
+
+  .feature-header:not(.feature-header__no-gap) {
+    margin-bottom: 0.75em;
+  }
+
+  .feature-header:after {
+    content: '';
+    display: block;
+    margin: 0.25em auto 0;
+    width: 0;
+    border-top: 3px solid currentColor;
+    transition: width 1200ms cubic-bezier(0.13, 0.88, 0.62, 1) 0.7s;
+  }
+
+  .feature-header--visible:after {
+    width: 36px;
+  }
+</style>
+<main id="page-content">
+  <div id="shopify-section-template--15078000459981__pageLargeTemplate" class="shopify-section">
+    <div data-section-type="page-large-template" class="large content">
+      <h1 class="feature-header feature-header--visible" data-cc-animate="">A French success story</h1>
+      <div class="rte" data-cc-animate="" data-cc-animate-delay="0.2s">
+        <div class="bloc-img1" style="text-align: center;">&nbsp;
+          <p><img class="desk" src="assets/images/img1.jpg" data-image="iahamf29u2ov"></p>
+        </div>
+        <div class="bloc-text" style="text-align: center;">
+          <h2><strong>1971</strong></h2>
+          <p>Danièle et Gerard Darel found the Gerard Darel house.</p>
+          <br>
+          <h2><strong>1987</strong></h2>
+          <p>The brand’s first shop opens its doors in Saint-Germain-des-Prés, where Parisians mingle with
+            artists and avant-gardists.</p>
+        </div>
+        <div class="bloc-img" style="text-align: center;">
+          <p><img src="assets/images/JackieKennedy.jpg" style="margin-top: 80px; float: none;"
+              data-image="4sxguoayadlp"></p>
+          &nbsp;
+          <h2><strong>1996</strong></h2>
+          <p>The brand purchases Jackie Kennedy’s famous necklace at Sotheby’s. Symbol of eternal feminine
+            sophistication, it becomes a classic.</p>
+        </div>
+        <div class="bloc-img" style="text-align: center;">&nbsp;
+          <p><img class="desk" src="assets/images/img3.jpg" data-image="70lqqgbf51vy"></p>
+        </div>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <div class="double-bloc" style="text-align: center;">
+          <div class="bloc-img2" style="text-align: left;">&nbsp;
+            <p><img class="desk" src="assets/images/img4.jpg"
+                style="margin-right: 20px; margin-bottom: 100px; float: left;" data-image="p5li79c6apdl"></p>
+          </div>
+          <div class="bloc-text">
+            <h2></h2>
+            <h2></h2>
+            <h2 style="text-align: left;">&nbsp;<strong>2003</strong>
+            </h2>
+            <p style="text-align: left;">Creation of the «24 heures» bag. It's an instant success and the
+              model has become iconic.</p>
+            <p>&nbsp;</p>
+          </div>
+          <br><br>
+        </div>
+        <p><br><br><br><br></p>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;"></div>
+        <div class="bloc-img" style="text-align: center;">
+          <p><img class="desk" src="assets/images/img5.jpg" data-image="7q91c5f45iu2"></p>
+        </div>
+        <div class="bloc-text" style="line-height: 36px;text-align: center;">
+          <p style="margin-top: 15px;">La maison Darel a acquis le mardi 27 mai 2003, l’original du fameux «Questionnaire» de Marcel Proust mis en vente aux enchères à Drouot Paris, France.
+Cette acquisition est un véritable coup de cœur pour un document touchant qui résume tout le charme, la sensibilité et l’esprit que représente l’œuvre de Marcel Proust.
+Autant de valeurs partagées avec la maison Darel depuis sa création en 1971.</p>
+        </div>
+        <p><br><br><br></p>
+        <div class="bloc-img" style="text-align: center;">
+          <p><img class="desk" src="assets/images/img6.jpg" data-image="nmlvqrgd1sdg"></p>
+        </div>
+        <div class="bloc-text" style="text-align: center;">
+          <h2><strong>2010</strong></h2>
+          <p>As a tribute to another legendary woman, the house re-produces the ecru pullover made famous by
+            Marilyn Monroe in the film «Let’s make love».<br>Winter 2010 media campaign, Robin Wright by
+            Peter Lindbergh</p>
+        </div>
+        <p><br><br><br></p>
+        <div class="bloc-img" style="text-align: center;">
+          <p><img class="desk" src="assets/images/img-gif.gif" data-image="b3ym3jxr5zil"></p>
+        </div>
+        <div class="bloc-text" style="text-align: center;">
+          <h2><strong>2017</strong></h2>
+          <p>Darel opens new stores worldwide: New York, Los Angeles, Barcelone, Londres...</p>
+        </div>
+        <p><br><br><br></p>
+        <div class="double-bloc" style="text-align: center;">
+          <div class="bloc-img3" style="text-align: left;">
+            <p><img class="desk" src="assets/images/img7.jpg" style="margin-right: 20px; float: left;" width="508"
+                height="535" data-image="cgy577zolk2l"></p>
+          </div>
+          <div class="last-bloc-text">
+            <h2 style="text-align: center;"></h2>
+            <h2 style="text-align: left;"><strong>&nbsp;2018</strong></h2>
+            <p style="text-align: left;">Spring-Summer 2018 media campaign, with Charlotte Gainsbourg.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<!-- Main::End-->
+<? require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
